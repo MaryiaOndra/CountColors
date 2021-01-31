314317073f9a6bd978033d4ef35bca0006467709
Maryia Ondra <ya.ondra@yandex.by>
2021-01-31 17:21:12 +0300

    added colors and shapes

---
+++ CountColors/Assets/Scripts/Figure.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace CountColors 
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Figure : MonoBehaviour
+    {
+        SpriteRenderer figureSpriteRndr;
+        Color[] colors;
+
+        public Color FigureColor { get; private set; }
+
+        void Start()
+        {
+            figureSpriteRndr = GetComponent<SpriteRenderer>();
+            colors = new Color[] { Color.white, Color.red, Color.green, Color.blue};
+            SetRandomColor();
+        }
+
+        void SetRandomColor()
+        {
+            int _colorIndx = Random.Range(0, colors.Length);
+            figureSpriteRndr.color = colors[_colorIndx];
+            FigureColor = figureSpriteRndr.color;
+        }
+    }
+}
---
+++ CountColors/Assets/Scripts/Generator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CountColors
+{
+    public class Generator : MonoBehaviour
+    {
+        const int ROWS = 5;
+        const int COLUMNS = 11;
+        const float CELL_SIZE = 1.5f;
+
+        [SerializeField] GameObject[] figurePrefabs;
+
+        int[,] figureMatrix;
+        [SerializeField] float offset = 0.15f;
+
+        void Start()
+        {
+            figureMatrix = new int[ROWS, COLUMNS];
+
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLUMNS; col++)
+                {
+                    figureMatrix[row, col] = Random.Range(0, 2);
+                }
+            }
+
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLUMNS; col++)
+                {
+                    bool _needCreate = Random.Range(0, 2) > 0;
+                    if (_needCreate)
+                    {
+                        int _figureIndx = Random.Range(0, figurePrefabs.Length);
+                        var _figureObj = Instantiate(figurePrefabs[_figureIndx], this.transform);
+
+                        Vector3 _randomOffcet = new Vector3(Random.Range(-offset, offset), Random.Range(-offset, offset));
+                        _figureObj.transform.position = new Vector3(CELL_SIZE * col, CELL_SIZE * row) + _randomOffcet + transform.position;
+                    }
+                }
+            }
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
